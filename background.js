94d86f33c2cbb578e64d55c74d9493f49884f79a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-09-24 20:24:09 -0500

    It works

--- background.js
+++ background.js
@@ -1,74 +1,40 @@
-
-
-
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(18, 1, 1, 1000);
-
-// const renderer = new THREE.WebGLRenderer({
-//   preserveDrawingBuffer: true,
-//   alpha: true
-// });
-// renderer.setSize(16, 16);
-
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({
-//   color: 0x7ff4ff,
-//   wireframe: true
-// });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-// const canvas = document.createElement("canvas");
-// canvas.width = 16;
-// canvas.height = 16;
-// const ctx = canvas.getContext("2d");
-
-// camera.position.z = 5;
-// cube.rotation.x = 0.5;
-
-// setInterval(() => {
-//   cube.rotation.y -= 0.02;
-//   renderer.render(scene, camera);
-
-//   const icon = renderFavicon();
-
-//   chrome.browserAction.setIcon({
-//     path: {
-//       "16": icon
-//     }
-//   });
-
-// }, 1000 / 60); // 60 fps maybe lmao
-
-// render loop
-// const renderFavicon = () => {
-//   ctx.clearRect(0, 0, 16, 16);
-//   ctx.drawImage(renderer.domElement, 0, 0);
-
-//   return canvas.toDataURL("image/png");
-// };
-
-// test skinview
 const canvas = document.createElement("canvas");
 
 const skinIcon = chrome.runtime.getURL("hacksore.png");
-console.log("icon", skinIcon);
 const skinViewer = new skinview3d.SkinViewer({
   canvas,
   width: 16,
   height: 16,
-  skin: skinIcon
+  skin: skinIcon,
+  preserveDrawingBuffer: true,
 });
+
+skinViewer.animations.add(skinview3d.RotatingAnimation);
+skinViewer.animations.add(skinview3d.WalkingAnimation);
 
 // render loop
 const renderFavicon = () => {
   const iconUrl = skinViewer.canvas.toDataURL("image/png");
-  console.log(iconUrl);
+
+  // move the canvas rotation
+  skinViewer.camera.rotation.x = -0.620;
+  skinViewer.camera.rotation.y = 0.534;
+  skinViewer.camera.rotation.z = 0.348;
+
+  // mvoe the camera position
+  skinViewer.camera.position.x = 6.5;
+  skinViewer.camera.position.y = 10;
+  skinViewer.camera.position.z = 12;
+
+  // draw
+  skinViewer.draw();
+
   chrome.browserAction.setIcon({
     path: {
       "16": iconUrl
     }
   });
+
 };
 
-setInterval(() => renderFavicon(), 1000);
+setInterval(() => renderFavicon(), 1000 / 60);
